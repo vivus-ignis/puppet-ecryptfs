41d136519f86e4826b28f02a1885fcbee16be102
Iaroslav Tarasenko <iaroslav.tarasenko@here.com>
2015-10-06 14:43:13 +0200

    node declaration removed from vagrant.pp

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,32 +1,30 @@
-node default {
-  file { '/tmp/passphrase':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => 'passphrase_passwd=foobarbazquux'
-  } ->
+file { '/tmp/passphrase':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0600',
+  content => 'passphrase_passwd=foobarbazquux'
+} ->
 
-  file { [ '/tmp/source', '/tmp/dest' ]:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  } ->
+file { [ '/tmp/source', '/tmp/dest' ]:
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+} ->
 
-  ecryptfs::mount { 'test ecryptfs mount':
-    source_dir      => '/tmp/source',
-    dest_dir        => '/tmp/dest',
-    passphrase_file => '/tmp/passphrase'
-  } ->
+ecryptfs::mount { 'test ecryptfs mount':
+  source_dir      => '/tmp/source',
+  dest_dir        => '/tmp/dest',
+  passphrase_file => '/tmp/passphrase'
+} ->
 
-  file { '/tmp/dest/test1':
-    content => 'foobarbaz'
-  } ->
+file { '/tmp/dest/test1':
+  content => 'foobarbaz'
+} ->
 
-  ecryptfs::mount { 'test ecryptfs unmount':
-    ensure          => unmounted,
-    source_dir      => '/tmp/source',
-    dest_dir        => '/tmp/dest',
-    passphrase_file => '/tmp/passphrase'
-  }
+ecryptfs::mount { 'test ecryptfs unmount':
+  ensure          => unmounted,
+  source_dir      => '/tmp/source',
+  dest_dir        => '/tmp/dest',
+  passphrase_file => '/tmp/passphrase'
 }
